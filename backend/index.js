133cdcb21dd9b06f6fa2d2ee92d68cd5b8538a4f
IN-Accurate <vishalsankar410@gmail.com>
2022-12-02 17:52:04 +0530

    Create react-app

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+const db = require('./models');
+
+//Routers - endpoints
+
+db.sequelize.sync().then(()=>{
+    
+    app.listen(process.env.PORT || 3001,() => {
+        console.log("Server running on port 3001");
+    })
+}).catch(err => {
+    console.error(err);
+});
