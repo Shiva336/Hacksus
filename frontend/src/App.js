2f19dacef344d37342e9d4007eb53647afff2fe9
IN-Accurate <vishalsankar410@gmail.com>
2022-12-03 01:51:27 +0530

    Add styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Map from './pages/Maps'
+import { BrowserRouter as Router, Route, Routes,Link } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App"> 
-    <Router>
-          <Routes>
-            <Route path="/" exact element={<Home/>} />
-          </Routes>
-    </Router>
+    <div className="App">  <Router>
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Home Page</Link>
+        <Link to="/maps"> Map Page</Link>
+      </div>
+    </div>
+    <Routes>
+      <Route path="/" exact element={<Home/>} />
+      <Route path="/maps" exact element={<Map/>} />
+    </Routes>
+  </Router>
     </div>
   );
 }
